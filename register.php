a81dfbcd3acc624b28ca6072eefaeebcebaf90e7
L-Y-V-C <148728120+L-Y-V-C@users.noreply.github.com>
2024-04-24 21:33:26 -0700

    Update register.php

--- register.php
+++ register.php
@@ -1,68 +1,62 @@
 <?php
-    include("connect.php");
+    $conn = new mysqli('localhost', 'root', '', 'dynwebpage');
     
     if(isset($_POST['register'])){
-        if(1<=strlen($_POST['Names'])){
         $names = trim($_POST['Names']);
         $surNames = trim($_POST['SurNames']);
         $email = trim($_POST['Email']);
         $phone = trim($_POST['Phone']);
 
-        /*$organizations = $_POST['organization'];
-        $jobs = $_POST['job'];
-        $institutions = $_POST['institution'];
-        $careers = $_POST['career'];
-        $languages = $_POST['language'];
-        $levels = $_POST['level'];*/
+        $stmt_insert_user = $conn->prepare("INSERT INTO 
+            maindata(names, surnames, email, phone) VALUES (?, ?, ?, ?)");
+        $stmt_insert_user->bind_param("ssss", $names, $surNames, $email, $phone);
+        $stmt_insert_user->execute();
 
-        $queryMaindata = "INSERT INTO 
-            maindata(names, surnames, email, phone) 
-            VALUES ('$names','$surNames','$email','$phone')";
-        /*$queryExperience = "INSERT INTO 
-            `experience`(`id`, `userid`, `organization`, `job`) 
-            VALUES ('[value-1]','[value-2]','[value-3]','[value-4]')";
-        $queryEducation = "INSERT INTO 
-            `education`(`id`, `userid`, `institution`, `career`) 
-            VALUES ('[value-1]','[value-2]','[value-3]','[value-4]')";
-        $queryLanguages = "INSERT INTO 
-            `languages`(`id`, `userid`, `language`, `level`) 
-            VALUES ('[value-1]','[value-2]','[value-3]','[value-4]')";
-        $querySkills = "INSERT INTO 
-            skills(userid, skill) 
-            VALUES ('$key','$')";*/
-
-        $go1 = mysqli_query($conn, $queryMaindata);
-        
-        /*foreach($_POST['skill'] as $key => $value){
-            $sql = "INSERT INTO 
-                skills(skl) 
-                VALUES (:skl)";
-            $stmt = $conn->prepare($sql);
-            $stmt->execute([
-                'skl' => $value
-            ]);
-        }*/
-
-        /*$skills = $_POST['skill'];
-        
-        $queryId = "SELECT id FROM maindata";
-        $go = mysqli_query($conn, $queryId);
-        $id = mysqli_fetch_all($go, MYSQLI_ASSOC);
-        print_r($id);
-        $key = max($id);
-        print_r($key);
-        foreach($skills as $skill){
-            $querySkills = "INSERT INTO 
-            skills(userid, skill) 
-            VALUES ('$key','$skill')";
-            $go2 = mysqli_query($conn, $querySkills);
-        }*/
-        
-        /*if($go1){
-            echo "Datos ingresados correctamente.";*
-        }else{
-            echo "Error al ingresar datos: ".mysqli_error($conn);
-        }*/
+        $userid = $stmt_insert_user->insert_id;
+        if(isset($_POST['organization'])){
+            foreach($_POST['organization'] as $index => $organization){
+                $job = $_POST['job'][$index];
+                $stmt_insert_experience = $conn->prepare("INSERT INTO 
+                    experience(userid, organization, job) 
+                    VALUES (?, ?, ?)");
+                $stmt_insert_experience->bind_param("iss", $userid, $organization, $job);
+                $stmt_insert_experience->execute();
+                $stmt_insert_experience->close();
+            }
+        }
+        if(isset($_POST['institution'])){
+            foreach($_POST['institution'] as $index => $institution){
+                $career = $_POST['career'][$index];
+                $stmt_insert_education = $conn->prepare("INSERT INTO 
+                    education(userid, institution, career) 
+                    VALUES (?, ?, ?)");
+                $stmt_insert_education->bind_param("iss", $userid, $institution, $career);
+                $stmt_insert_education->execute();
+                $stmt_insert_education->close();
+            }
+        }
+        if(isset($_POST['language'])){
+            foreach($_POST['language'] as $index => $language){
+                $level = $_POST['level'][$index];
+                $stmt_insert_languages = $conn->prepare("INSERT INTO 
+                    languages(userid, language, level) 
+                    VALUES (?, ?, ?)");
+                $stmt_insert_languages->bind_param("iss", $userid, $language, $level);
+                $stmt_insert_languages->execute();
+                $stmt_insert_languages->close();
+            }
+        }
+        if(isset($_POST['skill'])){
+            foreach($_POST['skill'] as $index => $skill){
+                $stmt_insert_skills = $conn->prepare("INSERT INTO 
+                    skills(userid, skill) 
+                    VALUES (?, ?)");
+                $stmt_insert_skills->bind_param("is", $userid, $skill);
+                $stmt_insert_skills->execute();
+                $stmt_insert_skills->close();
+            }
         }
     }
+    header("Location: index.php");
+    exit;
 ?>
